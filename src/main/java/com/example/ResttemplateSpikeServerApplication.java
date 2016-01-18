5843618b2494cada5a60189c81de6fe0d033b081
Leonhardt Koepsell <lkoepsell@pivotal.io>
2016-01-18 07:43:31 -0800

    initial commit

---
+++ src/main/java/com/example/ResttemplateSpikeServerApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttemplateSpikeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttemplateSpikeServerApplication.class, args);
+	}
+}
---
+++ src/main/resources/db/migration/V2__seed_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO "USER" (name) VALUES ('user 1');
+INSERT INTO "USER" (name) VALUES ('user 2');
+INSERT INTO "USER" (name) VALUES ('user 3');
+INSERT INTO "USER" (name) VALUES ('user 4');
